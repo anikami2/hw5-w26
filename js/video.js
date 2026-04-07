2cd5d0954e96fc7d2cfa5f0cbf545b2474f4c3de
anikami2 <anikami@umich.edu>
2026-04-07 18:12:44 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -19,9 +19,7 @@ document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 });
 
-// document.querySelector("#slider").addEventListener("input", function() {
-// 	const video = document.getElementById("player1")
-// });
+document.querySelector("#volume").innerHTML = video.volume * 100 +"%";
 
 // Pause the video.
 
@@ -33,13 +31,29 @@ document.querySelector("#pause").addEventListener("click", function() {
 
 // Slow the current video speed by 10% each time the button is clicked and log the new speed to the console. 
 
-document.querySelector("#slower").addEventListener("click", function() {
+document.getElementById("slower").addEventListener("click", function() {
 	// const video = document.getElementById("player1")
-	video.playbackRate = 0.10;
-	console.log("Slow video by 10%");
+	video.playbackRate *= 0.9;
+	console.log("New speed: " + video.playbackRate);
+});
+
+
+// Increase the current video speed each time the button is clicked and log the new speed to the console.  Change it by an amount proportional to the slow down. CHECK THIS!!  If you slow down three times and then speed up three times you should be within 5 digits of 100% again.
+document.getElementById("faster").addEventListener("click", function() {
+	// const video = document.getElementById("player1")
+	video.playbackRate /= 0.9;
+	console.log("New speed: " + video.playbackRate);
 });
 
 // Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start of the video - no farther.   Log the current location of the video.
+document.getElementById("skip").addEventListener("click", function() {
+	if (video.currentTime + 10 > video.duration) {
+		video.currentTime = 0;
+	} else {
+		video.currentTime += 10;
+	}
+	console.log("Current location: " + video.currentTime)
+});
 
 // Mute/unmute the video and update the text in the button.
 
@@ -55,5 +69,27 @@ button.addEventListener("click", function() {
 		button.innerText = "Unmute"
 	}
 });
+
+// Change the volume based on the slider and update the volume information.
+document.querySelector("#slider").addEventListener("input", function() {
+	video.volume = document.querySelector("#slider").value / 100;
+	document.querySelector("#slider").innerHTML = video.volume +"%";
+	console.log("Volume Information: " + video.volume);
+});
+
+// Utilize the existing oldSchool class on the video element
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("vintage");
+	console.log("Old school is added");
+});
+
+// Remove the oldSchool class from the video.
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.add("orig");
+	console.log("Old school is removed");
+});
+
+
+
 
 
